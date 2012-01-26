2c327893e5f17ba9c800b59b5e162f695c2415e1
Spencer Steffen <spencer@citrusme.com>
2012-01-26 12:08:36 -0800

    add simplecov and ensure coverage is ignored by git

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,20 @@
 ENV["RAILS_ENV"] = "test"
 
+gem "minitest"
+
+begin
+  require "simplecov"
+  SimpleCov.start do
+    add_filter do |source_file|
+      source_file.filename =~ /dummy/
+    end
+  end
+rescue LoadError
+end
+
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 require "minitest/autorun"
 require "minitest/should"
-require "sqlite3"
 
 Rails.backtrace_cleaner.remove_silencers!
