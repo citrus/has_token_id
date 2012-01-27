c8d48992237a601f5bb0ee0c4695efa52599ff70
Spencer Steffen <spencer@citrusme.com>
2012-01-26 17:08:49 -0800

    fix before migrate hook while gsubbing the migration

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,17 +1,17 @@
+# Generate an Item model
 run "rails g scaffold item token:string name:string"
 
+# Add has_token_id to our Item model
 gsub_file "app/models/item.rb", "end", %(
   has_token_id
   
 end)
 
+# Ensure our migration is using our `token` TableDefinition
+@migration = File.basename(Dir[File.expand_path("db/migrate/*.rb", destination_path)].last)
+gsub_file File.join("db/migrate/", @migration), "t.string :token", "t.token"
 
-puts destination_path.inspect
-
-migration = Dir.entries(File.join(destination_path, "db/migrate")).last
-
-gsub_file File.join("db/migrate/", migration), "t.string :token", "t.token"
-
+# Route items#index as the root path
 gsub_file "config/routes.rb", "resources :items", %(
   resources :items
   root :to => "items#index")
